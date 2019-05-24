99fc52e051390718717d867b8ccf07d2637bc4ca
BeINone <sbeinone@gmail.com>
2019-05-25 05:18:45 +0900

    config server 추가

---
+++ abler-config/src/test/java/io/github/binaitz/ablerconfig/AblerConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.binaitz.ablerconfig;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AblerConfigApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
