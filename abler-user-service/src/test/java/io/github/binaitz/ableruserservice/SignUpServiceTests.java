e8917b5a877a06574a6166414557e8029c253339
BeINone <sbeinone@gmail.com>
2019-06-08 00:20:26 +0900

    resolve #3 회원가입 API 추가
    - ID 중복 test code 추가

--- abler-user-service/src/test/java/io/github/binaitz/ableruserservice/SignUpServiceTests.java
+++ abler-user-service/src/test/java/io/github/binaitz/ableruserservice/SignUpServiceTests.java
@@ -1,16 +1,25 @@
 package io.github.binaitz.ableruserservice;
 
 import io.github.binaitz.ableruserservice.domain.User;
+import io.github.binaitz.ableruserservice.domain.UserAlreadyExistsException;
+import io.github.binaitz.ableruserservice.domain.UserRepository;
 import io.github.binaitz.ableruserservice.service.SignUpService;
 import io.github.binaitz.ableruserservice.web.SignUpRequest;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.BDDMockito.given;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -22,18 +31,40 @@ public class SignUpServiceTests {
     @Autowired
     private SignUpService signUpService;
 
+    @MockBean
+    private UserRepository userRepository;
+
     @Test
     public void signUp() {
-        SignUpRequest signUpRequest = SignUpRequest.builder()
-                .id(USER_ID)
+        // given
+        SignUpRequest signUpRequest = signUpRequest();
+
+        given(userRepository.findByUserId(USER_ID)).willReturn(Optional.empty());
+
+        // when
+        signUpService.signUp(signUpRequest);
+    }
+
+    @Test(expected = UserAlreadyExistsException.class)
+    public void signUpWithConflictingId() {
+        // given
+        SignUpRequest signUpRequest = signUpRequest();
+
+        User user = User.builder()
+                .userId(USER_ID)
                 .password(PASSWORD)
                 .build();
 
-        User user = signUpService.signUp(signUpRequest);
+        given(userRepository.findByUserId(USER_ID)).willReturn(Optional.of(user));
 
-        assertNotNull(user);
-        assertNotNull(user.getId());
-        assertEquals(user.getUserId(), USER_ID);
-        assertEquals(user.getPassword(), PASSWORD);
+        // when
+        signUpService.signUp(signUpRequest);
+    }
+
+    private SignUpRequest signUpRequest() {
+        return SignUpRequest.builder()
+                .id(USER_ID)
+                .password(PASSWORD)
+                .build();
     }
 }
